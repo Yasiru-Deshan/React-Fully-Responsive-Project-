e7c33977e3107f987373225d00e153d5b19d65f3
yassa98 <yasirudeshan3@gmail.com>
2021-06-16 02:57:14 +0530

    Home Page

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,68 @@
+export const homeObjOne = {
+    id:'about',
+    lightBg: false,
+    lightText:true,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headline: 'Unlimited Transactions with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: require('../../images/svg-1.svg').default,
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+}
+
+export const homeObjTwo = {
+    id:'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc:false,
+    topLine: 'Unlimited Access',
+    headline: 'Unlimited Transactions with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+    buttonLabel: 'Learn More',
+    imgStart: true,
+    img: require('../../images/svg-2.svg').default,
+    alt: 'Girllap',
+    dark: false,
+    primary: false,
+    darkText: true
+}
+
+export const homeObjThree = {
+    id:'',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headline: 'Unlimited Transactions with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: require('../../images/svg-3.svg').default,
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+}
+
+
+export const homeObjFour = {
+    id:'signup',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc:true,
+    topLine: 'Unlimited Access',
+    headline: 'Unlimited Transactions with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+    buttonLabel: 'Learn More',
+    imgStart: true,
+    img: require('../../images/svg-4.svg').default,
+    alt: 'Girllap',
+    dark: true,
+    primary: true,
+    darkText: false
+}
